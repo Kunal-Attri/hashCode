cc2e2bda0a923e25e4305f04ff6b98d5fd5c2c1f
Kunal Attri <attrikunal16@gmail.com>
2022-02-25 00:14:31 +0530

    try

--- example.py
+++ example.py
@@ -6,9 +6,16 @@ inp = f.readlines()
 inp = [i.strip() for i in inp]
 inp = [i.split(" ") for i in inp]
 
+contributors = inp[0][0]
 projects = inp[0][1]
+inp.pop(0)
 
+users = []
+skills = {}
 
-
+for i in range(contributors):
+    users.append(inp[0][0])
+    skills[users[i]] = []
+    for i in range()
 
 
